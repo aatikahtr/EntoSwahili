a8baee20d1f426010b559cda5685b2bb21481ce4
aatikahtr <074313k@gmail.com>
2026-05-08 23:12:41 +0300

    Update Jlb.py

--- tg_bot/Jlb.py
+++ tg_bot/Jlb.py
@@ -147,8 +147,8 @@ async def get_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
             # Tafuta main content container
             content_selectors = [
 #                "article",
-                ".entry-content",
-#                ".post-content",
+#                ".entry-content",
+                ".post-content",
 #                ".article-content",
 #                "main article",
 #                ".single-content",
