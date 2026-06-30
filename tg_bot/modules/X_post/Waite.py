b53b520d8093923c6f8379a6e43e1644e58ccab5
aatikahtr <074313k@gmail.com>
2026-06-30 15:32:41 +0300

    Add files via upload

---
+++ tg_bot/modules/X_post/Waite.py
@@ -0,0 +1,94 @@
+import re
+import asyncio
+from telegram import Update
+from telegram.ext import ContextTypes
+from .New_update import x_update
+
+URL_REGEX = re.compile(r'(https?://[^\s]+)')
+Knowledge_translate = -1004358606228
+ERROR_CHAT_ID = -1003754038608
+
+async def textzote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        msg = update.channel_post or update.edited_channel_post
+        if not msg:
+            return
+
+        chat_id = msg.chat.id
+        text = msg.text or ""
+
+        if any(word in text for word in ["R to @", "RT by @", "RT @"]):
+            await context.bot.delete_message(
+                chat_id=chat_id,
+                message_id=msg.message_id
+            )
+            return
+
+        urls = URL_REGEX.findall(text)
+        if not urls:
+            return
+
+        new_url = urls[-1]
+
+        if chat_id == Knowledge_translate:
+            asyncio.create_task(x_update(update, context, new_url, chat_id))
+            return
+
+    except Exception as e:
+        error_msg = f"Kosa limetokea kwenye function ya TEXT: {e}"
+        asyncio.create_task(
+            context.bot.send_message(
+                chat_id=ERROR_CHAT_ID,
+                text=error_msg
+            )
+        )import re
+import asyncio
+from telegram import Update
+from telegram.ext import ContextTypes
+from.New_update import x_update
+
+# -----------------------------------
+# REGEX PATTERNS
+# -----------------------------------
+URL_REGEX = re.compile(r'(https?://[^\s]+)')
+Knowledge_translate = -1004358606228
+
+# -----------------------------------
+# TEXT HANDLER
+# -----------------------------------
+async def textzote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        msg = update.channel_post or update.edited_channel_post
+        if not msg:
+            return
+        
+        chat_id = message.chat.id
+        text = msg.text or ""
+        
+        if any(word in text for word in ["R to @", "RT by @",  "RT @"]):
+            await context.bot.delete_message(
+                chat_id=chat_id,
+                message_id=message.message_id
+            )
+            return
+        
+        urls = URL_REGEX.findall(text)
+        if not urls:
+            return
+        
+        new_url = urls[-1]
+        
+        if chat_id == Knowledge_translate:
+            asyncio.create_task(x_update(update, context, new_url, chat_id))
+            return
+        
+        
+        
+    except Exception as e:
+        error_msg = f"Kosa limetokea kwenye function ya TEXT: {e}"
+        asyncio.create_task(
+            context.bot.send_message(
+                chat_id=error_id,
+                text=error_msg
+            )
+        )
